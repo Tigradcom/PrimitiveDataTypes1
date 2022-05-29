c263a13974ac99742964ddee8d4b8c7cf3b6c967
Tigradcom <tigradcom2@gmail.com@>
2022-05-29 22:21:24 +0400

    AddPrimitiveDataTypes1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 2_746;
+        int oneMiles = 20;
+
+        int miles = priceTicket / oneMiles;
+        System.out.println("Бонусные мили:" + miles);
+    }
+}
